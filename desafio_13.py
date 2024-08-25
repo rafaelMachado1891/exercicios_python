d6c2d84f78f62293495133cc1e565fce03d3d3d7
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 20:45:12 -0300

    resolucao desafio 13

---
+++ desafio_13.py
@@ -0,0 +1,4 @@
+# faca um programa q receba o salario de funcionario e retorne o novo salario com 15% de aumento
+salario = float(input('qual é o salario do funcionário? '))
+resultado = salario * 1.15
+print('o salario do funcionario com o aumento de 15% é de {:.2f}'.format(resultado))
