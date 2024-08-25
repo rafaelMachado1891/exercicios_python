12f4b94e4dd962eeec8589cbe928aacd1648c5fd
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 15:39:37 -0300

    resolucao desafio_05

---
+++ desafio_05.py
@@ -0,0 +1,5 @@
+# faça um programa que receba um número e apresente seu antecessor e sucessor
+n = int(input('digite um numero: '))
+n1= n-1
+n2 = n+1
+print('o antecessor de {} é {} e o sucessor é {}'.format(n, n1, n2))
