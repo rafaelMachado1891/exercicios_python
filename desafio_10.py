db95a7590927c5fbbb073f72eb81c21e04370dfa
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 18:27:42 -0300

    atualização do desafio

--- desafio_10.py
+++ desafio_10.py
@@ -1,4 +1,4 @@
-# crie um programa que faz a cotação de um determinado valor
+# crie um programa que diga ao usuario quantos dolares ele pode adiquirir utilizando a cotação de 3.27
 v = float(input('digite o valor que  você tem na sua carteira: '))
-resultado = v * 3.27
+resultado = v // 3.27
 print(resultado)
