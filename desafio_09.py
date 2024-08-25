a19e562e8221f37ccac20154f7dda5792972a6e2
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 17:55:57 -0300

    resolucao desafio_09

---
+++ desafio_09.py
@@ -0,0 +1,23 @@
+# escreva um programa q retorne a tabuada de um numero
+
+n = int(input('digite um numero para retornar a sua tabuada: '))
+r1 = n * 1
+r2 = n * 2
+r3 = n * 3
+r4 = n * 4
+r5 = n * 5
+r6 = n * 6
+r7 = n * 7
+r8 = n * 8
+r9 = n * 9
+r10 = n * 10
+print('{} x 1 = {}'.format(n,r1))
+print('{} x 2 = {}'.format(n,r2))
+print('{} x 3 = {}'.format(n,r3))
+print('{} x 4 = {}'.format(n,r4))
+print('{} x 5 = {}'.format(n,r5))
+print('{} x 6 = {}'.format(n,r6))
+print('{} x 7 = {}'.format(n,r7))
+print('{} x 8 = {}'.format(n,r8))
+print('{} x 9 = {}'.format(n,r9))
+print('{} x 10 = {}'.format(n,r10))
