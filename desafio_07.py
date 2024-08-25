aa5010ebc4faca6ec26cd9d60febf8b324e79715
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 15:56:18 -0300

    resolucao do exercicio_07

---
+++ desafio_07.py
@@ -0,0 +1,5 @@
+# desenvolva um programa que receba duas notas de um aluno e calcule a media 
+nota1 = float(input('digite a 1º nota: '))
+nota2 = float(input('digite a 2º nota: '))
+media = (nota1 + nota2) / 2 
+print('a media das notas {} e {} é {}'. format(nota1, nota2, media))
