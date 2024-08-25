cfe354b64f4f61b2758e47f78e18276bc5c72e3f
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-25 20:39:57 -0300

    resolucao desafio 12

---
+++ desafio_12.py
@@ -0,0 +1,4 @@
+# crie um programa que calcule o preço do produto com 5% de desconto
+preco = float(input('qual é o preco do produto? '))
+resultado = preco * 0.95
+print('o preco do produto com desconto é {:.2f}'.format(resultado))
