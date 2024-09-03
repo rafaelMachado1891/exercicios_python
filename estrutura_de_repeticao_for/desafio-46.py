b71a38c37b167b14322bf40a62cf56564edd1cff
rafaelMachado1891 <rafadmn1@gmail.com>
2024-09-02 22:16:53 -0300

    resolucao dos exercicios

---
+++ estrutura_de_repeticao_for/desafio-46.py
@@ -0,0 +1,6 @@
+# faça um program que mostre na tela a contagem regressiva para o estouro de fogos de artificio com intervalo de 1 segundo entre eles
+import time
+for i in range(10,-1,-1):
+    print(i)
+    time.sleep(1)
+print('Acabou! Fim! ')
