4b1a987a9dc552fd79203975b03c0ebf909e15e0
rafaelMachado1891 <rafadmn1@gmail.com>
2024-09-16 20:59:56 -0300

    resolucao desafio-69

--- estrutura_de_repeticao_while/desafio-69.py
+++ estrutura_de_repeticao_while/desafio-69.py
@@ -1,3 +1,28 @@
-resto = int(10+8) % 2
-res = resto
-print(res)
+# crie um programa que leia a idade e o sexo de varias pessoas. A cada pessoa cadastrada o programa 
+# deve perguntar ao usuário se ele quer continuar. No final mostre, quantas pessoas tem mais de 18 anos, quantos homens 
+# foram cadastrados e quantas mulheres tem menos de 20 anos.
+
+m = f = idade = feminino = 0
+soma = m + f
+resposta = 'S'
+
+while resposta == 'S':
+    i = int(input('Digite a idade: '))
+    sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
+    while sexo not in 'MF':
+       sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
+    resposta = str(input('Você desejas continuar o cadastro? [S/N]: ')).upper().strip()[0]
+    while resposta not in 'SN':
+       resposta = str(input('Você desejas continuar o cadastro? [S/N]: ')).upper().strip()[0]
+    if sexo in 'M':
+       m+=1
+    else:
+       f+=1
+    soma+=1
+    if i >= 18:
+       idade+=1
+    if i < 18 and sexo in 'F':
+       feminino+=1
+
+print(f'foram cadastrados {soma} pessoas sendo {m} homens, {idade} pessoas maiores de 18 anos e {feminino} mulheres menores que 18 anos')
+    
