c3632da1e5f86ca982e56bd73d7f16395e94d854
rafaelMachado1891 <rafadmn1@gmail.com>
2024-10-04 15:33:11 -0300

    desafio-79

---
+++ listas/desafio-79.py
@@ -0,0 +1,19 @@
+# Crie um programa onde o usuário possa digita varios valores numericos e cadastre-os em uma lista
+# Caso o numero já exista lá dentro ele não será adicionado.
+# No final, serão exibidos todos os valores únicos digitados em ordem crescente
+
+r = 'S'
+lista = []
+v = 0
+
+while True:
+    if r == 'N':
+        break
+    else:
+        v = int(input('Digite um número: '))
+        if v in lista:
+            lista.append(v)
+        else:
+            pass
+    r = str(input('Deseja adcionar mais valores [S/N]: ')).upper().strip()[0]
+print(f'{lista}')
