fbc6929f8259ebf1ce5a430928f024bd263ea170
rafaelMachado1891 <rafadmn1@gmail.com>
2024-10-31 16:49:33 -0300

    resolucao desafio 97

---
+++ funcoes/desafio-97.py
@@ -0,0 +1,15 @@
+# Crie um programa personalizado em que a linha de formatação acompanhe o comprimento do texto! 
+def formatar_linhas_texto(texto):
+   var = '-'
+   tam = len(texto)
+   res = var * tam
+   print(res)
+   print(texto)
+   print(res)
+   
+   
+variavel = str(input('Escreva: '))
+
+formatar_linhas_texto(variavel)
+
+
