a12d6091d04a28fe55f70fd769d40fa15d9b50be
rafaelMachado1891 <rafadmn1@gmail.com>
2024-11-05 07:46:25 -0300

    desafio-98

---
+++ funcoes/desafio-98.py
@@ -0,0 +1,28 @@
+# faça um programa que tenha uma função chamada contador, a função deve exibir receber tres parametros.
+# inicio, fim, e passo. A funcção deve retornar 3 resultados, 1º 1 a 10 de 1 em 1, de 10 a 1 de 2 a 2 
+# e um resultado onde o usuário digite inicio fim e passo! 
+from time import sleep
+
+
+def contador(i, f, p):
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(1)
+    
+    if i < f:
+        cont=i
+        while cont <= f:
+            print(f'{cont} ', end='',flush=True)
+            sleep(0.5)
+            cont+=p
+        print('Fim!')
+    else:
+        cont = i
+        while cont>=f:
+            print(f'{cont} ', end='',flush=True)
+            sleep(0.5)
+            cont-=p
+        print('Fim!')
+
+
+contador(1,10,1)
+contador(10,0,2)
