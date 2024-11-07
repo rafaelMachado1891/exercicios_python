f3f3898e27ff80a4cb29539c07d2b4e2a8e12396
rafaelMachado1891 <rafadmn1@gmail.com>
2024-11-06 21:09:42 -0300

    resolucao desafio 100

---
+++ funcoes/desafio-100.py
@@ -0,0 +1,38 @@
+# Faça uma programa que contenha uma lista chamada numeros e duas funções chamada sorteia e soma par
+# a primeira função vai sortear 5 valores e coloca-los na lista e a segunda vai somar apenas os valores pares! 
+
+from random import randint
+from time import sleep
+
+def somar_valores_pares(lista):
+    valor = 0
+    pos = 0
+    for i, v in enumerate(lista):
+        if v % 2 ==0:
+            valor+=v
+    print()
+    print('-='*20)     
+    print(f'A soma dos valores pares é {valor}')
+
+
+def sortear_valores_lista(lista):
+    for i in range(0,6):
+        n=randint(1, 60)
+        lista.append(n)
+    print(f'Sorteando....',end=' ',flush=True)
+    sleep(0.5)
+
+    pos=0
+
+    for i in (lista):
+        print(f'{lista[pos]}', end=' ',flush=True)
+        sleep(0.5)
+        pos+=1
+
+
+lista = []
+
+
+sortear_valores_lista(lista)   
+somar_valores_pares(lista)
+
