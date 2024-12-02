a34206713394f06624bd468d9a74b2d07b59d82e
rafaelMachado1891 <rafadmn1@gmail.com>
2024-12-01 21:06:52 -0300

    update desafio 103

--- funcoes/desafio-103.py
+++ funcoes/desafio-103.py
@@ -2,12 +2,23 @@
 # o nome de um jogador e quantos gols ele marcou. 
 # o progama deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente
 
-def ficha(a=0, b=0):    # parametro opcional para declarar podemos informar valor como 0
-    a=str(input('Jogados: '))
-    b=int(input('Gols: '))    
-    print(f'Jogador: {a}')
-    print(f'Numero de Gols: {b}')
-    return a,b
+def ficha(jog='<desconhecido>', gol=0):    # parametro opcional para declarar podemos informar valor como 0
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato')
+    
+    
+j=str(input('Jogador: '))
+g=str(input('numero de gols: '))
+if g.isnumeric():
+   g = int(g)
+else:
+   g = 0
+if j.strip()=='':
+   ficha(gol=g)
+else:
+   ficha(j,g)
+
+
+
 
 
 
