0509781d85183e8a336cf53ca1a02eece605b937
rafaelMachado1891 <rafadmn1@gmail.com>
2024-12-05 12:40:24 -0300

    uodate desafio-105

--- funcoes/desafio-105.py
+++ funcoes/desafio-105.py
@@ -6,17 +6,24 @@
 # A média da turma
 # A situação 
 
-def notas(*p):
+def notas(*p, sit=False):      # 
     v = {
         'total notas': len(p),
         'maior nota': max(p),
         'menor nota': min(p),
         'media': sum(p) / len(p)
     }
+    if sit:
+        if v['media'] >=7:
+            v['situação'] = 'BOA'
+        elif v['media'] >= 5:
+            v['situação'] = 'RAZOAVEL'
+        else:
+            v['situação'] = 'RUIM'
     
     return v
 
 
-n = notas(5.5, 6, 4.3, 2)
+n = notas(5.5, 6, 4.3, 2, 7, 8.8, 10, 9.75, 3.2, 7.4, 10, sit=True)
 
 print(n)
