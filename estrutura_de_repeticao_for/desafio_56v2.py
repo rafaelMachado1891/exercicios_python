39c3960e0a8d8da9f66e1fa0b6d5df5ceb308004
rafaelMachado1891 <rafadmn1@gmail.com>
2025-01-09 23:09:54 -0300

    resolução desafio-56v2.py

---
+++ estrutura_de_repeticao_for/desafio_56v2.py
@@ -0,0 +1,11 @@
+# faça um programa que leia 6 numero inteiros e realize a soma somente dos numeros pares! 
+
+from random import randint
+
+lista = []
+
+for i in range(1,7):
+    a = randint(1,60)
+    lista.append(a)
+
+print(lista)
