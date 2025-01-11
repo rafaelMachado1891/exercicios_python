cc802656af560318a14fbfa510737f61b2630025
rafaelMachado1891 <rafadmn1@gmail.com>
2025-01-10 22:16:30 -0300

    update desafio 56v2.py

--- estrutura_de_repeticao_for/desafio_56v2.py
+++ estrutura_de_repeticao_for/desafio_56v2.py
@@ -3,9 +3,14 @@
 from random import randint
 
 lista = []
+par = 0
 
 for i in range(1,7):
     a = randint(1,60)
     lista.append(a)
+    if a % 2 == 0:
+        par+=a
+    
 
-print(lista)
+print(f' os numeros gerados foram {lista}')
+print(f' a soma dos números pares da lista é {par}')
